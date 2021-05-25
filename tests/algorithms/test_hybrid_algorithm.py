11c2a2ea5281aa428aece96f70823ceb4d0c1f97
yairkoskas <yairi2003@gmail.com>
2021-05-25 14:10:03 +0300

    added real test for parse_csv

--- tests/algorithms/test_hybrid_algorithm.py
+++ tests/algorithms/test_hybrid_algorithm.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 from algorithms.HybridAlgorithm import HybridAlgorithm
-
+from algorithms.algorithm_utils import *
 
 def test_parse_csv():
     path = "C:/Users/yairi/OneDrive/Desktop/University/AP2/AP2-Ex2/tests/reg_flight.csv"
-    d = HybridAlgorithm._parse_csv(path)
-    print(d.keys())
-    print(len(d.keys()))
-    print(d['latitude-deg'])
+    d = parse_csv(path)
+    assert len(d) == 42
+    assert d['aileron'] == 2174
 
 tests = {'test_parse_csv': test_parse_csv}
 
