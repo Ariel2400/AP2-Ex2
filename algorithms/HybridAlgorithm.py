623990d8a0875395e86fbb7620b38bc5149912af
yair koskas <yairi2003@gmail.com>
2021-05-25 19:55:36 +0300

    removed the server managment from algorithm

--- algorithms/HybridAlgorithm.py
+++ algorithms/HybridAlgorithm.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from algorithms.algorithm_utils import *
 import pandas as pd
+import socket
 import json
-# import pyod
 import os
 
 CONFIG_PATH = os.getcwd() + '/config'
@@ -11,4 +11,12 @@ CONFIG_PATH = os.getcwd() + '/config'
 class HybridAlgorithm:
 
     def __init__(self, path: str):
-        self.data = parse_csv(path)
+        self.sock = socket.socket()
+
+    def train_model(self):
+        with open(CONFIG_PATH + '/anomaly_detection_config.json', 'r') as ip_config:
+            d = json.load(ip_config)
+            self.sock.connect((d['ip'], int(d['port'])))
+        self.sock.recv(2048)
+        # todo train model
+
