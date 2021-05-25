33a793b9a3b057fef3b74c1ca787a0a3a812308a
yairkoskas <yairi2003@gmail.com>
2021-05-25 14:10:28 +0300

    removed parse_csv from algorithm class

--- algorithms/HybridAlgorithm.py
+++ algorithms/HybridAlgorithm.py
@@ -1,22 +1,14 @@
 from pathlib import Path
+from algorithms.algorithm_utils import *
 import pandas as pd
 import json
+# import pyod
 import os
 
 CONFIG_PATH = os.getcwd() + '/config'
 
+
 class HybridAlgorithm:
 
-    def __init__(self, path: Path):
-        self.data = self._parse_csv(path)
-
-    @staticmethod
-    def _parse_csv(path: str) -> dict:
-        with open(CONFIG_PATH + '/algorithms/column_titles.json', 'r') as titles_config:
-            titles = list(json.load(titles_config).keys())
-        df = pd.read_csv(path)
-        row_as_df = pd.DataFrame(list(df.columns), list(df.columns))
-        df = row_as_df.transpose().append(df)
-        df.columns = titles
-        d = df.to_dict()
-        return d
+    def __init__(self, path: str):
+        self.data = parse_csv(path)
