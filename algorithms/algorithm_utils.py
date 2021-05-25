db33d77ea220f255b3443bde4d597d649c6daecf
yair koskas <yairi2003@gmail.com>
2021-05-25 19:41:26 +0300

    added start_server method

--- algorithms/algorithm_utils.py
+++ algorithms/algorithm_utils.py
@@ -15,3 +15,15 @@ def parse_csv(path: str) -> dict:
     d = df.to_dict()
     return d
 
+
+def start_server():
+    path_to_AP1_Code = os.getcwd() + '/AP1_Code'
+    if 'a.out' not in os.listdir(path_to_AP1_Code):
+        oldpwd = os.getcwd()
+        os.chdir(path_to_AP1_Code)
+        os.system('make')
+        os.chdir(oldpwd)
+    with open(CONFIG_PATH + '/anomaly_detection_config.json', 'r') as ip_config:
+        d = json.load(ip_config)
+        os.system(f'{path_to_AP1_Code}/a.out {int(d["port"])} &')
+        print('hello')
