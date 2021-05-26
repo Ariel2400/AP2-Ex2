3cbc9da9dbc29ab1e2aebe0bae7406693d1ecc1a
Ariel2400 <ariel241202@gmail.com>
2021-05-26 19:39:21 +0300

    finished html form handeling in flask

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 import os
 from flask import Flask, render_template, request,redirect,flash
 from werkzeug.utils import secure_filename
-UPLOAD_DIR = "uploads"
+UPLOAD_DIR = 'uploads'
+
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    # target = os.path.join(app.root_path, "uploads")
-    # if not os.path.isdir(target):
-    #     os.makedirs(target)
     if request.method == "POST":
-    #     f=request.files['reg_csv']
-    #     f_name=f.filename
-    #     dest='/'.join([target,f_name])
-    #     f.save(dest)
-        # print('check')
-
-
-        #in order to det the algorithm name
+        file_irreg = request.files['reg_csv']
+        file_irreg.save(secure_filename(file_irreg.filename))
+        file_reg = request.files['irreg_csv']
+        file_reg.save(secure_filename(file_reg.filename))
         print(f"Chosen algorithm: {request.form.get('algorithms')}")
         
     
