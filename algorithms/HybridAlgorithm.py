4d21f62b1341651f006c9e01d2143c1b81bb5747
yair koskas <yairi2003@gmail.com>
2021-05-27 01:47:29 +0300

    added detect_anomalies method which returns a json object

--- algorithms/HybridAlgorithm.py
+++ algorithms/HybridAlgorithm.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from algorithms.algorithm_utils import *
+from algorithms.BaseAlgorithm import BaseAlgorithm
 import pandas as pd
 import socket
 import json
@@ -8,15 +9,21 @@ import os
 CONFIG_PATH = os.getcwd() + '/config'
 
 
-class HybridAlgorithm:
+class HybridAlgorithm(BaseAlgorithm):
 
     def __init__(self, path: str):
-        self.sock = socket.socket()
+        super().__init__(path)
 
-    def train_model(self):
-        with open(CONFIG_PATH + '/anomaly_detection_config.json', 'r') as ip_config:
-            d = json.load(ip_config)
-            self.sock.connect((d['ip'], int(d['port'])))
-        self.sock.recv(2048)
-        # todo train model
+    def detect_anomalies(self):
+        sock = super().detect_anomalies()
+        sock.send(b'3\n') # anomaly detection with hybrid algorithm
+        sock.send(b'5\n')
+        b = sock.recv(100000).decode() # get anomaly detection results
+        d = dict()
+        for line in b.split('\n'):
+            time, desc = line.split('\t')[0], line.split('\t')[1]
+            d[time] = desc
+        return json.dumps(d)
+
+
 
