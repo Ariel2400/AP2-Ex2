1f23f4355f66ec1875730ce641dbab5975e4f6e8
Ariel2400 <ariel241202@gmail.com>
2021-05-30 15:27:37 +0300

    included API

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 import json, os
+from pprint import pprint
+
+import requests
 from flask import Flask, render_template, request
 from flask_restful import Api, Resource
 
 from algorithms.LinearAlgorithm import LinearAlgorithm
 from algorithms.HybridAlgorithm import HybridAlgorithm
-
+BASE = 'http://127.0.0.1:5000/detect'
 app = Flask(__name__)
 api = Api(app)
 
-#the post request to detect anomalies:
+
+# the post request to detect anomalies:
 class MY_API(Resource):
 
-    def post(self):
+    def post(self) -> str:
         algo_type = request.json['algo_type']
-        correct_csv = request.json['correct_csv']
-        false_csv = request.json['false_csv']
-        if algo_type == "HybridAlgorithm":
+        correct_csv = request.json['reg_csv']
+        false_csv = request.json['irreg_csv']
+        if algo_type == "hybrid":
             x = HybridAlgorithm(correct_csv, false_csv)
         else:
             x = LinearAlgorithm(correct_csv, false_csv)
+
         return x.detect_anomalies()
 
+
 api.add_resource(MY_API, "/detect")
-
-
-
 
 UPLOAD_DIR = os.getcwd() + '/uploads'
 if not os.path.exists(UPLOAD_DIR):
@@ -38,10 +41,16 @@ def index():
         if not (save_file_to_uploads('reg_csv') and save_file_to_uploads('irreg_csv')):
             return render_template('check.html', display_data=False, result={})
         # debug
+        reg_path = os.path.join(app.config['UPLOAD_FOLDER'], request.files['reg_csv'].filename)
+        irreg_path = os.path.join(app.config['UPLOAD_FOLDER'], request.files['irreg_csv'].filename)
         print(f"Chosen algorithm: {request.form.get('algorithms')}")
+        algo_type = request.form.get('algorithms')
         # here's the function that processes the data and returns json_res
-        json_res = test_json()
-        dict_res = json.loads(json_res)
+        # json_res = api.post()
+        response = requests.post(BASE, json={'algo_type': algo_type, 'reg_csv': reg_path, 'irreg_path': irreg_path})
+        res_json = response.json()
+        # json_res = test_json()
+        dict_res = json.loads(res_json)
         return render_template('check.html', display_data=True, result=dict_res)
 
     return render_template('check.html', display_data=False, result={})
