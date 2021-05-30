a660475dc86d85a3a47534cc55dcd46b7e762c88
Itay Gradenwits <itay25433@gmail.com>
2021-05-30 14:27:37 +0300

    added to app.py the ability to post request into '/detect/

--- app.py
+++ app.py
@@ -1,39 +1,66 @@
-import json,os
+import json, os
 from flask import Flask, render_template, request
+from flask_restful import Api, Resource
 
+from algorithms.LinearAlgorithm import LinearAlgorithm
+from algorithms.HybridAlgorithm import HybridAlgorithm
 
 app = Flask(__name__)
-UPLOAD_DIR = os.getcwd() +'/uploads'
+api = Api(app)
+
+#the post request to detect anomalies:
+class MY_API(Resource):
+
+    def post(self):
+        algo_type = request.json['algo_type']
+        correct_csv = request.json['correct_csv']
+        false_csv = request.json['false_csv']
+        if algo_type == "HybridAlgorithm":
+            x = HybridAlgorithm(correct_csv, false_csv)
+        else:
+            x = LinearAlgorithm(correct_csv, false_csv)
+        return x.detect_anomalies()
+
+api.add_resource(MY_API, "/detect")
+
+
+
+
+UPLOAD_DIR = os.getcwd() + '/uploads'
 if not os.path.exists(UPLOAD_DIR):
     os.mkdir(UPLOAD_DIR)
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         if not (save_file_to_uploads('reg_csv') and save_file_to_uploads('irreg_csv')):
             return render_template('check.html', display_data=False, result={})
-        #debug
+        # debug
         print(f"Chosen algorithm: {request.form.get('algorithms')}")
-        #here's the function that processes the data and returns json_res
-        json_res=test_json()
-        dict_res=json.loads(json_res)
-        return render_template('check.html', display_data = True, result=dict_res)
+        # here's the function that processes the data and returns json_res
+        json_res = test_json()
+        dict_res = json.loads(json_res)
+        return render_template('check.html', display_data=True, result=dict_res)
 
-    
     return render_template('check.html', display_data=False, result={})
 
-def save_file_to_uploads(name:str)->bool :
+
+def save_file_to_uploads(name: str) -> bool:
     try:
         file_from_request = request.files[name]
-        file_path=os.path.join(app.config['UPLOAD_FOLDER'],file_from_request.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_from_request.filename)
         file_from_request.save(file_path)
         return True
     except:
         return False
 
+
 def test_json():
-    x={"success":"yes","failure":"no"}
+    x = {"success": "yes", "failure": "no"}
     return json.dumps(x)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
