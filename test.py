d007d1f0412cbabd2bfd50a57dbd00b5f2b25e27
Ariel2400 <ariel241202@gmail.com>
2021-05-30 19:39:40 +0300

    deleted unnecessary test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-response = requests.post(BASE, json={"algo_type": "hybrid",
-                                     "correct_csv": {'key1': [1, 2, 3], 'key2': [4, 5, 6]},
-                                     "false_csv": 2})
-print(response.json())
